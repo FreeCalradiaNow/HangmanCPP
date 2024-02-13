05e708e75c3831aecd5f223f93c40995f3caf3f6
Michel <111363541+FreeCalradiaNow@users.noreply.github.com>
2024-02-13 01:55:58 +0100

    - Cleaned up (#20)
    - Deleted gap()

--- Menu.cpp
+++ Menu.cpp
@@ -8,27 +8,6 @@
 
 using namespace std;
 
-// Style elements
-//string mFHeader = "----->   FREECALRADIAs BORING HANGMAN CPP   <-----";
-//string mFSidebarL = "|  ";
-//string mFSidebarR = "  |";
-//string crlf = "\n";
-//string tab = "\t";
-
-// 7 times CRLF
-//void gap() {
-//    for (int i = 0; i < 8; i++)
-//    {
-//        cout << "\n";
-//    }
-//}
-
-// Reuseable textblocks
-//string legoText1 = "Guess the word: ";
-//string legoText2 = " You still have ";
-//string legoText3 = " failed attempts left.";
-//string legoText4;
-
 string getLegoText4(bool isTrySuccessfull) {
     if (isTrySuccessfull) {
         legoText4 = "Nice job - what took you so long?";
